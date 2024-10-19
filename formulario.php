1d7aa8ab65ef1c11efd0a8711ea882304ee2fe88
Paulo <94388255+paulolesilva@users.noreply.github.com>
2024-10-19 14:45:57 +0000

    criando formulando

---
+++ formulario.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+<form action="formulario.php" method="post">
+        <label>Digite seu nome:</label>
+        <input type="text" name="nome"/>
+
+        <input type="submit" value="Enviar para o servidor">
+    </form>
+    <br>
+    <hr/>
+
+<?php
+    $nome = '';
+
+    //Olá, <nome>! Seja bem-vindo(a).
+
+    if(isset($_POST['nome'])){ //isset é uma função do PHP que verifica se há valor em uma variável
+        
+        //estamos atribuindo o valor do campo "nome" digitado no formulário
+        //à nossa variável chamada $nome
+        $nome = $_POST['nome']; 
+
+        //Estamos imprimindo a mensagem de boas vindas. 
+        //Concatenamos uma string constante com a variável "$nome"
+        echo '<p>Olá, '. $nome .'! Seja bem-vindo(a)</p>';
+    }
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 08</title>
+</head>
+<body>
+<h1>Introdução à programação backend</h1>
+<?php
+
+    echo    'Olá, José';
+    $ano_nascimento = 1992;
+    $ano_atual =2024;
+    $idade = $ano_atual - $ano_nascimento;
+
+    echo 'Você possui '. $idade . ' anos.'
+
+    ?>
+    <br>
+    <a href="formulario.php">Ir para o formulário</a>
+    
+</body>
+</html>
